1c2cf568ac4455865a71464f4cd9435920a432d7
ktg <masterplexdev@gmail.com>
2024-07-08 00:22:27 +0900

    3초 뒤 슬립

--- cicd.bat
+++ cicd.bat
@@ -1,3 +1,3 @@
 
-start powershell.exe -ExecutionPolicy Bypass -Command "& {.\local-build-and-run.ps1; Read-Host 'Press Enter to exit'}"
+start powershell.exe -ExecutionPolicy Bypass -Command "& {.\local-build-and-run.ps1; Start-Sleep -Seconds 3}"
 exit
