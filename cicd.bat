bc1b3469462d742f332cb34d9a087ac46c3379f1
ktg <ktg219@mz.co.kr>
2024-07-12 07:36:06 +0900

    TEST

--- cicd.bat
+++ cicd.bat
@@ -1,3 +1,3 @@
 
-start powershell.exe -ExecutionPolicy Bypass -Command "& {.\local-build-and-run.ps1; Start-Sleep -Seconds 3}"
+start powershell.exe -ExecutionPolicy Bypass -Command "& {.\local-build-and-run.ps1}"
 exit
