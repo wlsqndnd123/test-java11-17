fa4b5b1a7b3d3ab8a0232913906eda62a1ee29b9
jin <wlsqndnd123@naver.com>
2024-07-12 12:34:06 +0900

    커밋

--- src/main/java/com/example/SimpleWebApp.java
+++ src/main/java/com/example/SimpleWebApp.java
@@ -15,6 +15,6 @@ public class SimpleWebApp {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, World(진짜 수정 - 수정한다!)";
+        return "Hello, World(진짜 수정 - 수정한다1!)";
     }
 }
